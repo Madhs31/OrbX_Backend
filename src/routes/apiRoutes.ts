525a97f9c0db25e15ff3734d45f4e49353ce42da
Madhs31 <fernandinha.mf30@gmail.com>
2025-11-18 19:25:25 -0300

    🛣️ chore: Mapeando novas rotas

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -3,8 +3,13 @@ import {
     getContinents, createContinent, getContinentById, 
     getCountries, createCountry, getCountryById, 
     getCities, createCity, getCityById, 
-    getDashboardStats 
+    getDashboardStats,
+    updateContinent, deleteContinent,
+    updateCountry, deleteCountry,
+    updateCity, deleteCity
 } from '../controllers/geoController.js';
+
+import { authenticateToken } from '../middleware/authMiddleware.js'; 
 import { 
     getCountryInfo, 
     getWeather,
@@ -16,24 +21,37 @@ const router = Router();
 // Continentes
 router.get('/continents', getContinents);
 router.get('/continents/:id', getContinentById); 
-router.post('/continents', createContinent); 
 
 // Países
 router.get('/countries', getCountries);
 router.get('/countries/:id', getCountryById); 
-router.post('/countries', createCountry); 
 
 // Cidades
 router.get('/cities', getCities);
 router.get('/cities/:id', getCityById); 
-router.post('/cities', createCity); 
 
-// Stats
+// Status
 router.get('/stats/dashboard', getDashboardStats); 
+
+// Continentes
+router.post('/continents', authenticateToken, createContinent); 
+router.put('/continents/:id', authenticateToken, updateContinent); 
+router.delete('/continents/:id', authenticateToken, deleteContinent); 
+
+// Países
+router.post('/countries', authenticateToken, createCountry);
+router.put('/countries/:id', authenticateToken, updateCountry);
+router.delete('/countries/:id', authenticateToken, deleteCountry);
+
+// Cidades
+router.post('/cities', authenticateToken, createCity);
+router.put('/cities/:id', authenticateToken, updateCity);
+router.delete('/cities/:id', authenticateToken, deleteCity);
+
 
 // Rotas Externas
 router.get('/external/country/:countryName', getCountryInfo); 
 router.get('/external/weather', getWeather);
-router.get('/external/image', getPexelsImage); // <-- ADICIONADO
+router.get('/external/image', getPexelsImage); 
 
 export default router;
